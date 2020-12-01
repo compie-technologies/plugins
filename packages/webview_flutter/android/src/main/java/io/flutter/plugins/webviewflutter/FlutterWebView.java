229abb6f332a01c17b32191841d0f7dde3532156
Danielle Kahana <danielle.k@compie.co.il>
2020-12-01 14:47:03 +0200

    setDefaultFixedFontSize to 16

--- packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
+++ packages/webview_flutter/android/src/main/java/io/flutter/plugins/webviewflutter/FlutterWebView.java
@@ -95,7 +95,7 @@ public class FlutterWebView implements PlatformView, MethodCallHandler {
     webView.getSettings().setDomStorageEnabled(true);
     webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 
-    webView.getSettings().setDefaultFontSize(16);
+    webView.getSettings().setDefaultFixedFontSize(16);
 
     // Multi windows is set with FlutterWebChromeClient by default to handle internal bug: b/159892679.
     webView.getSettings().setSupportMultipleWindows(true);
